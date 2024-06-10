894ccf95849fd78b49a0e5d8c94a0471eccb4d72
Ali-Umed <aliumed844@gmail.com>
2024-06-10 14:33:46 +0300

    feature: submmit order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,6 +3,8 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
+import { getCart } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -41,8 +43,12 @@ function CreateOrder() {
   const isSubmitting = navigation.state === 'submitting';
 
   const formErrors = useActionData();
-  const cart = fakeCart;
+  // const cart = fakeCart;
+  const cart = useSelector(getCart);
 
+  if (!cart.length) {
+    return <EmptyCart />;
+  }
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
@@ -99,6 +105,7 @@ function CreateOrder() {
         </div>
 
         <div>
+          {/* because we can not data in redux  */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting} type="primary">
             {isSubmitting ? 'Placing order....' : 'Order now'}
@@ -128,11 +135,8 @@ export async function action({ request }) {
 
   // If everything is okay, create new order and redirect
 
-  // const newOrder = await createOrder(order);
-
-  // return redirect(`/order/${newOrder.id}`);
-
-  return null;
+  const newOrder = await createOrder(order);
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
